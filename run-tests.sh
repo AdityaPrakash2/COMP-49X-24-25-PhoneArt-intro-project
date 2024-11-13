34eb5d09f79d51a9d7b5a63c66d93cf4f7654f5a
AdityaPrakash2 <adityaprakash2003.ap@gmail.com>
2024-11-12 22:04:44 -0800

    Created Script File

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCHEME='COMP-49X-24-25-PhoneArt-intro-project'
-DESTINATION='platform=iOS Simulator,name=iPhone 16 Pro'
+DESTINATION='platform=iOS Simulator,OS=18.0,name=iPhone 16 Pro'
 xcodebuild test -scheme $SCHEME -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO'
