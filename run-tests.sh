c7dcdcfc0c6f206120424bafef3872f7e50d40de
AdityaPrakash2 <adityaprakash2003.ap@gmail.com>
2024-11-12 18:12:22 -0800

    Removing files

--- run-tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SCHEME='COMP-49X-24-25-PhoneArt-intro-project'
-DESTINATION='platform=iOS Simulator,name=iPhone 16 Pro'
-xcodebuild test -scheme $SCHEME -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO'
