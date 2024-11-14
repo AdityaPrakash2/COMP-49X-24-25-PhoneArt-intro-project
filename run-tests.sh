abdf3ab05771f4c30ad548bf638dfa65e1e77701
AdityaPrakash2 <adityaprakash2003.ap@gmail.com>
2024-11-13 17:33:12 -0800

    Testing Workflow

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 SCHEME='COMP-49X-24-25-PhoneArt-intro-project'
-DESTINATION='platform=iOS Simulator,OS=18.1,name=iPhone 16 Pro'
+DESTINATION='platform=iOS Simulator,OS=18.1,name=iPhone 16'
 xcodebuild -project COMP-49X-24-25-PhoneArt-intro-project.xcodeproj -scheme $SCHEME -destination "$DESTINATION" build test CODE_SIGNING_ALLOWED='NO'
+killall "iOS Simulator"
